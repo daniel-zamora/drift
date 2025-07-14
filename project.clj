eca86b7f485f54f9d4647c133842176a82c5bb2a
daniel-zamora <daniel.p.zamora@nasa.gov>
2025-07-14 12:31:03 -0400

    updates clojure-tools

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 ;; IMPORTANT: When bumping the version number here, be sure to bump it also in
 ;; src/drift/drift_version.clj! 
-(defproject drift "1.5.4-SNAPSHOT"
+(defproject org.clojars.daniel-zamora/drift "1.5.5-SNAPSHOT"
   :description "Drift is a rails like migration framework for Clojure."
-  :dependencies [[clojure-tools "1.1.2"]
+  :dependencies [[org.clojars.daniel-zamora/clojure-tools "1.1.5-SNAPSHOT"]
                  [org.clojure/tools.logging "0.2.3"]]
   :profiles {:dev {:dependencies [[log4j/log4j "1.2.16"]]}}
 

7f1a3491a11372084bcef7418c78aa7363fbe4cc
daniel-zamora <daniel.p.zamora@nasa.gov>
2025-07-15 08:30:42 -0400

    updates license and gitignore

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
 (defproject org.clojars.daniel-zamora/drift "1.5.5-SNAPSHOT"
   :description "Drift is a rails like migration framework for Clojure."
   :license {:name "Apache License, Version 2.0"
-            :url "http://www.apache.org/licenses/LICENSE-2.0"} 4
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojars.daniel-zamora/clojure-tools "1.1.5-SNAPSHOT"]
                  [org.clojure/tools.logging "0.2.3"]]
   :profiles {:dev {:dependencies [[log4j/log4j "1.2.16"]]}}
